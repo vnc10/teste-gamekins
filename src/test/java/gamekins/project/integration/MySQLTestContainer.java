27740ceca6425fa468277e110528a9159083f457
Vinicius Petris <viniciusbosa@gmail.com>
2025-12-07 15:48:19 -0300

    fix: my sql test container

--- src/test/java/gamekins/project/integration/MySQLTestContainer.java
+++ src/test/java/gamekins/project/integration/MySQLTestContainer.java
@@ -1,27 +1,32 @@
 package gamekins.project.integration;
 
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MySQLContainer;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
-@Testcontainers
-public class MySQLTestContainer {
+public abstract class MySQLTestContainer {
 
-    private static final MySQLContainer<?> MYSQL_CONTAINER;
+    static final MySQLContainer<?> MYSQL_CONTAINER;
 
     static {
         MYSQL_CONTAINER = new MySQLContainer<>("mysql:8.0")
                 .withDatabaseName("testdb")
                 .withUsername("test")
                 .withPassword("test");
-        MYSQL_CONTAINER.start();
 
-        System.setProperty("spring.datasource.url", MYSQL_CONTAINER.getJdbcUrl());
-        System.setProperty("spring.datasource.username", MYSQL_CONTAINER.getUsername());
-        System.setProperty("spring.datasource.password", MYSQL_CONTAINER.getPassword());
-        System.setProperty("spring.datasource.driver-class-name", "com.mysql.cj.jdbc.Driver");
+        MYSQL_CONTAINER.start();
     }
 
-    public static MySQLContainer<?> getInstance() {
-        return MYSQL_CONTAINER;
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+
+        registry.add("spring.datasource.url", MYSQL_CONTAINER::getJdbcUrl);
+        registry.add("spring.datasource.username", MYSQL_CONTAINER::getUsername);
+        registry.add("spring.datasource.password", MYSQL_CONTAINER::getPassword);
+        registry.add("spring.datasource.driver-class-name", MYSQL_CONTAINER::getDriverClassName);
+
+        registry.add("spring.flyway.url", MYSQL_CONTAINER::getJdbcUrl);
+        registry.add("spring.flyway.user", MYSQL_CONTAINER::getUsername);
+        registry.add("spring.flyway.password", MYSQL_CONTAINER::getPassword);
     }
 }
